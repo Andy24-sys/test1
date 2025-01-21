3ca908ff926176a059c36424dcc1fdf9556b1d18
Andy24-sys <balans8bak@mail.ru>
2025-01-21 18:37:04 +0300

    vtr-18:36

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Новая строка");
+import java.util.HashSet;
 
+    public class Main {
+
+        public static void main(String[] args) {
+            Solution solution = new Solution();
+            System.out.println(solution.containsDuplicate(new int[]{1, 2, 3, 1})); // Output: true
+//            System.out.println(solution.containsDuplicate(new int[]{1, 2, 3, 4})); // Output: false
+        }
     }
-}
+
