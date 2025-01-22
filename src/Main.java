afe60836aae5db39a0008da35cee1b68e68dcf76
Andy24-sys <balans8bak@mail.ru>
2025-01-22 13:07:41 +0300

    sout false

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
         public static void main(String[] args) {
             Solution solution = new Solution();
             System.out.println(solution.containsDuplicate(new int[]{1, 2, 3, 1})); // Output: true
-//            System.out.println(solution.containsDuplicate(new int[]{1, 2, 3, 4})); // Output: false
+            System.out.println(solution.containsDuplicate(new int[]{1, 2, 3, 4})); // Output: false
         }
     }
 
